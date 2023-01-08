18c0624388624195677232f3c7312e3e3f4426ad
drs317lakshami <devrajkumar317@gmail.com>
2023-01-08 14:51:21 +0530

    Added :

---
+++ src/com/bridgelabz/functionalprograms/VowelOrConsonant.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.functionalprograms;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Alphabet: ");
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+		VowelOrConsonant vc = new VowelOrConsonant();
+		vc.alphabet(ch);
+	}
+
+	public void alphabet(char ch) {
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println("Vowel is: " + ch);
+		} else {
+			System.out.println("Consonant is: " + ch);
+		}
+	}
+}
